5ca5de50c5b4dc422fd4a15af1d0caa8458f0ad1
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-03-27 00:12:20 -0600

    Preloader

--- js/script.js
+++ js/script.js
@@ -49,6 +49,7 @@ $(document).ready(function() {
 // Icon Animation
 
 $(document).ready(function() {
+   setTimeout(function(){ 
     $(".r").each(function() {
         var element = $(this);
         var deg = element.data('rot');
@@ -73,7 +74,9 @@ $(document).ready(function() {
             duration: 1500,
         })
     });
+    }, 6000);
 });
+
 
 // Parallax Effects
 
@@ -88,7 +91,8 @@ $('.parallaxie').parallaxie({
 
 AOS.init();
 
-// ===== Scroll to Top ==== 
+// Scroll to Top Widget
+
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) {    // If page is scrolled more than 50px
         $('#top').fadeIn("fast");       // Fade in the arrow
@@ -100,4 +104,40 @@ $('#top').click(function() {            // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                   // Scroll to top of body
     }, 500);
+});
+
+
+// Simple Page Loader
+
+var cssIdowl = 'myCss';
+if (!document.getElementById(cssIdowl))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssIdowl;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/static/css/preloader.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
+
+var p = document.createElement("div");
+p.innerHTML = "<div style='z-index:100000' id='owlreporter-preloader'><div id='loader'></div><div class='loader-section section-left'></div><div class='loader-section section-right'></div></div>";
+document.body.insertBefore(p, document.body.firstChild);
+
+function pageload() {
+    var e = (new Date).getTime(),
+        t = (e - before) / 1e3,
+        n = document.getElementById("loadingtime");
+    n.innerHTML = "Page load: " + t + " seconds."
+}
+window.onload = function() {
+    pageload()
+}, setTimeout(function() {
+    document.body.className += " loaded"
+}, 6000), document.addEventListener ? document.addEventListener("DOMContentLoaded", function() {
+    document.removeEventListener("DOMContentLoaded", arguments.callee, !1), domReady()
+}, !1) : document.attachEvent && document.attachEvent("onreadystatechange", function() {
+    "complete" === document.readyState && (document.detachEvent("onreadystatechange", arguments.callee), domReady())
 });
