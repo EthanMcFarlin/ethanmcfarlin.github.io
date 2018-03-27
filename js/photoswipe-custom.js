3142c32ddf9566681f3bca95b18d09a5471c5f8b
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-03-26 22:34:20 -0600

    Projects section

---
+++ js/photoswipe-custom.js
@@ -0,0 +1,165 @@
+var AopenPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
+            w: 964,
+            h: 1024
+        },
+        {
+            src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
+            w: 1024,
+            h: 683
+        }
+    ];
+    
+    // define options (if needed)
+    var options = {
+			 // history & focus options are disabled on CodePen        
+      	history: false,
+      	focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+        
+    };
+    
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
+
+var BopenPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: 'https://www.gettyimages.com/detail/photo/sensual-woman-in-white-sexy-lingerie-on-white-bed-royalty-free-image/525363700?esource=SEO_GIS_CDN_Redirect',
+            w: 964,
+            h: 1024
+        },
+        {
+            src: 'https://cdn.shopify.com/s/files/1/0747/3829/products/mNS0490_41183d68-b16f-4065-a0d7-252fa01697c4_1024x1024.jpg?v=1487864046',
+            w: 1024,
+            h: 683
+        }
+    ];
+    
+    // define options (if needed)
+    var options = {
+			 // history & focus options are disabled on CodePen        
+      	history: false,
+      	focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+        
+    };
+    
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
+
+var CopenPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: 'https://www.gettyimages.com/detail/photo/sensual-woman-in-white-sexy-lingerie-on-white-bed-royalty-free-image/525363700?esource=SEO_GIS_CDN_Redirect',
+            w: 964,
+            h: 1024
+        },
+        {
+            src: 'https://cdn.shopify.com/s/files/1/0747/3829/products/mNS0490_41183d68-b16f-4065-a0d7-252fa01697c4_1024x1024.jpg?v=1487864046',
+            w: 1024,
+            h: 683
+        }
+    ];
+    
+    // define options (if needed)
+    var options = {
+			 // history & focus options are disabled on CodePen        
+      	history: false,
+      	focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+        
+    };
+    
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
+var DopenPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: 'https://www.gettyimages.com/detail/photo/sensual-woman-in-white-sexy-lingerie-on-white-bed-royalty-free-image/525363700?esource=SEO_GIS_CDN_Redirect',
+            w: 964,
+            h: 1024
+        },
+        {
+            src: 'https://cdn.shopify.com/s/files/1/0747/3829/products/mNS0490_41183d68-b16f-4065-a0d7-252fa01697c4_1024x1024.jpg?v=1487864046',
+            w: 1024,
+            h: 683
+        }
+    ];
+    
+    // define options (if needed)
+    var options = {
+			 // history & focus options are disabled on CodePen        
+      	history: false,
+      	focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+        
+    };
+    
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
+var EopenPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: 'https://www.gettyimages.com/detail/photo/sensual-woman-in-white-sexy-lingerie-on-white-bed-royalty-free-image/525363700?esource=SEO_GIS_CDN_Redirect',
+            w: 964,
+            h: 1024
+        },
+        {
+            src: 'https://cdn.shopify.com/s/files/1/0747/3829/products/mNS0490_41183d68-b16f-4065-a0d7-252fa01697c4_1024x1024.jpg?v=1487864046',
+            w: 1024,
+            h: 683
+        }
+    ];
+    
+    // define options (if needed)
+    var options = {
+			 // history & focus options are disabled on CodePen        
+      	history: false,
+      	focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+        
+    };
+    
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
+
+document.getElementById('open1').onclick = AopenPhotoSwipe;
+document.getElementById('open2').onclick = BopenPhotoSwipe;
+document.getElementById('open3').onclick = CopenPhotoSwipe;
+document.getElementById('open4').onclick = DopenPhotoSwipe;
+document.getElementById('open5').onclick = EAopenPhotoSwipe;
+
+
