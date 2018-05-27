57098153e3316458ff0adf44e935bc05f2b5d2a8
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-05-27 12:26:33 -0600

    Miscellaneous information update

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
             duration: 1500,
         })
     });
-    }, 3000);
+    }, 0);
 });
 
 // Animate on Scroll
@@ -73,41 +73,6 @@ $('#top').click(function() {            // When arrow is clicked
     }, 500);
 });
 
-
-// Simple Page Loader
-
-var cssIdowl = 'myCss';
-if (!document.getElementById(cssIdowl))
-{
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    link.id   = cssIdowl;
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = '/static/css/preloader.css';
-    link.media = 'all';
-    head.appendChild(link);
-}
-
-var p = document.createElement("div");
-p.innerHTML = "<div style='z-index:100000' id='owlreporter-preloader'><div id='loader'></div><div class='loader-section section-left'></div><div class='loader-section section-right'></div></div>";
-document.body.insertBefore(p, document.body.firstChild);
-
-function pageload() {
-    var e = (new Date).getTime(),
-        t = (e - before) / 1e3,
-        n = document.getElementById("loadingtime");
-    n.innerHTML = "Page load: " + t + " seconds."
-}
-window.onload = function() {
-    pageload()
-}, setTimeout(function() {
-    document.body.className += " loaded"
-}, 3000), document.addEventListener ? document.addEventListener("DOMContentLoaded", function() {
-    document.removeEventListener("DOMContentLoaded", arguments.callee, !1), domReady()
-}, !1) : document.attachEvent && document.attachEvent("onreadystatechange", function() {
-    "complete" === document.readyState && (document.detachEvent("onreadystatechange", arguments.callee), domReady())
-});
 
 // Progress Bar
 
